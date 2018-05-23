c274bc60db72f558f6a83e7b76c0d46c38e3c535
d-dasha <daryadernovaya@gmail.com>
2018-05-24 00:44:33 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="main-container">
+				<div className="title">function is not defined</div>
+			</div>
+		);
+	}
+}
+
+export default App;
