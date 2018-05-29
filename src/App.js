0da9f5deab1f2f7f3a2817329b44f7c4dd03d22a
d-dasha <daryadernovaya@gmail.com>
2018-05-29 21:23:59 +0300

    n

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import './style.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="main-container">
-				<div className="title">function is not defined</div>
+				<div className="title title--animation">function is not defined</div>
 			</div>
 		);
 	}
